9b8724f0129718dfe2fe14e627a7b303faab3067
grndstt <pierre-marie.allard@unige.ch>
2023-10-03 19:20:21 +0200

    defining some objects

---
+++ 02_enpkg_taxo_enhancer/tests/test_lineage_appender.py
@@ -0,0 +1,21 @@
+from src.abstract_taxon_metadata import AbstractTaxonMetadata
+from src.taxo_resolver import taxa_lineage_appender
+
+class TaxonMetadataPatch(AbstractTaxonMetadata):
+
+    def get_taxon_name(self) -> str:
+        """
+        Returns the taxon name of the sample.
+        """
+        return "Arabidopsis thaliana"
+
+
+def test_taxon_name():
+    taxon_metadata_patch = TaxonMetadataPatch()
+    assert taxon_metadata_patch.get_taxon_name() == "Arabidopsis thaliana"
+
+def test_taxa_lineage_appender():
+    taxon_metadata_patch = TaxonMetadataPatch()
+    taxo_df = taxa_lineage_appender(
+        taxon_metadata_patch, True, "path_to_results_folders", "directory"
+    )
