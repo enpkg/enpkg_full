1159924b3f73286fcf6d13592cc70747ff716192
grndstt <pierre-marie.allard@unige.ch>
2023-10-18 10:01:25 +0200

    smaller toyds

--- tests/utils.py
+++ tests/utils.py
@@ -9,10 +9,10 @@ def retrieve_zenodo_data():
     """Retrieve the data from Zenodo."""
     downloader = BaseDownloader()
     downloader.download(
-        "https://zenodo.org/record/8152039/files/dbgkg_tropical_toydataset.tar.gz?download=1",
-        "./data/input/dbgkg_tropical_toydataset.tar.gz",
+        "https://zenodo.org/records/10016507/files/dbgi_tropical_mini_toy_dataset.zip?download=1",
+        "./data/input/dbgi_tropical_mini_toy_dataset.zip",
     )
     # ALSO EXTRACT ALL OF THE THINGS!
     extractor = AutoExtractor(delete_original_after_extraction=True)
-    paths = glob("./data/input/**/*.gz", recursive=True)
+    paths = glob("./data/input/**/*.zip", recursive=True)
     extractor.extract(paths)
