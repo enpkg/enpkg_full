38dab28213bf4234ec33a2ff6c71fa81b61ca545
grndstt <pierre-marie.allard@unige.ch>
2023-10-18 12:24:11 +0200

    03-05 params

--- 06_enpkg_graph_builder/src/individual_processing/03_rdf_csi_annotations_indi.py
+++ 06_enpkg_graph_builder/src/individual_processing/03_rdf_csi_annotations_indi.py
@@ -18,25 +18,40 @@ from hash_functions import get_hash, get_data
 p = Path(__file__).parents[2]
 os.chdir(p)
 
-""" Argument parser """
-parser = argparse.ArgumentParser(
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    description=textwrap.dedent('''\
-        This script generate a RDF graph (.ttl format) from samples' individual SIRIUS annotations
-         --------------------------------
-            Arguments:
-            - Path to the directory where samples folders are located
-            - Ionization mode to process
-        '''))
 
-parser.add_argument('-p', '--sample_dir_path', required=True,
-                    help='The path to the directory where samples folders to process are located')
-parser.add_argument('-ion', '--ionization_mode', required=True,
-                    help='The ionization mode to process')
+# Loading the parameters from yaml file
 
-args = parser.parse_args()
-sample_dir_path = os.path.normpath(args.sample_dir_path)
-ionization_mode = args.ionization_mode
+
+if not os.path.exists('config/params.yaml'):
+    print('No config/params.yaml: copy from config/template.yaml and modify according to your needs')
+with open(r'config/params.yaml') as file:
+    params_list = yaml.load(file, Loader=yaml.FullLoader)
+
+# Parameters can now be accessed using params_list['level1']['level2'] e.g. params_list['options']['download_gnps_job']
+
+
+# """ Argument parser """
+# parser = argparse.ArgumentParser(
+#     formatter_class=argparse.RawDescriptionHelpFormatter,
+#     description=textwrap.dedent('''\
+#         This script generate a RDF graph (.ttl format) from samples' individual SIRIUS annotations
+#          --------------------------------
+#             Arguments:
+#             - Path to the directory where samples folders are located
+#             - Ionization mode to process
+#         '''))
+
+# parser.add_argument('-p', '--sample_dir_path', required=True,
+#                     help='The path to the directory where samples folders to process are located')
+# parser.add_argument('-ion', '--ionization_mode', required=True,
+#                     help='The ionization mode to process')
+
+# args = parser.parse_args()
+
+
+sample_dir_path = os.path.normpath(params_list['sample_dir_path'])
+output_format = params_list['graph_format']
+ionization_mode = params_list['ionization_mode']
 
 g = Graph()
 nm = g.namespace_manager
@@ -44,9 +59,10 @@ nm = g.namespace_manager
 with open(os.path.normpath('data/adducts_formatter.json')) as json_file:
     adducts_dic = json.load(json_file)
 
-kg_uri = "https://enpkg.commons-lab.org/kg/"
+kg_uri = params_list['kg_uri']
 ns_kg = rdflib.Namespace(kg_uri)
-prefix = "enpkg"
+prefix = params_list['prefix']
+
 nm.bind(prefix, ns_kg)
 
 path = os.path.normpath(sample_dir_path)
@@ -100,8 +116,8 @@ for directory in tqdm(samples_dir):
         
     pathout = os.path.join(sample_dir_path, directory, "rdf/")
     os.makedirs(pathout, exist_ok=True)
-    pathout = os.path.normpath(os.path.join(pathout, f'sirius_{ionization_mode}.ttl'))
-    g.serialize(destination=pathout, format="ttl", encoding="utf-8")
+    pathout = os.path.normpath(os.path.join(pathout, f'sirius_{ionization_mode}.{output_format}'))
+    g.serialize(destination=pathout, format=output_format, encoding="utf-8")
     
     # Save parameters:
     params_path = os.path.join(sample_dir_path, directory, "rdf", "graph_params.yaml")
