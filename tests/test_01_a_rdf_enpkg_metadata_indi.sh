eff720bf532a6869f95a15a375c60942cad84c6e
grndstt <pierre-marie.allard@unige.ch>
2023-10-18 11:14:22 +0200

    updates tests sirius and isdb

--- tests/test_01_a_rdf_enpkg_metadata_indi.sh
+++ tests/test_01_a_rdf_enpkg_metadata_indi.sh
@@ -3,6 +3,6 @@ conda activate graph_builder
 
 python ./06_enpkg_graph_builder/src/individual_processing/01_a_rdf_enpkg_metadata_indi.py \
 -p ../tests/data/output \
--f nt
+-f ttl
 
 conda deactivate
