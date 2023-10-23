909fc806787716ea286b0a9346ea700c80aef598
grndstt <pierre-marie.allard@unige.ch>
2023-10-23 12:20:29 +0200

    up

--- tests/test_data_organization.py
+++ tests/test_data_organization.py
@@ -5,5 +5,5 @@ from .utils import retrieve_zenodo_data
 def test_data_organization():
     """Test whether the data organization is correct."""
     # First, we retrieve the data from Zenodo if we have not done so already.
-    # retrieve_zenodo_data(record_id="10018590", record_name="enpkg_toy_dataset.zip")
-    retrieve_zenodo_data()
+    retrieve_zenodo_data(record_id="10018590", record_name="enpkg_toy_dataset.zip")
+    # retrieve_zenodo_data()
--- tests/utils.py
+++ tests/utils.py
@@ -5,30 +5,30 @@ from downloaders import BaseDownloader
 from downloaders.extractors import AutoExtractor
 
 
-def retrieve_zenodo_data():
-    """Retrieve the data from Zenodo."""
-    downloader = BaseDownloader()
-    downloader.download("https://zenodo.org/records/10018590/files/enpkg_toy_dataset.zip?download=1","./data/input/enpkg_toy_dataset.zip")
-    # ALSO EXTRACT ALL OF THE THINGS!
-    extractor = AutoExtractor(delete_original_after_extraction=True)
-    paths = glob("./data/input/**/*.zip", recursive=True)
-    extractor.extract(paths)
-
-# Same function, with Zenodo record id as parameter
-
-# def retrieve_zenodo_data(record_id, record_name):
-#     """Retrieve the data from Zenodo.
-#     Parameters
-#     ----------
-#     record_id : str
-#         The Zenodo record ID.
-#     record_name : str
-#         The Zenodo record name.
-#     """
+# def retrieve_zenodo_data():
+#     """Retrieve the data from Zenodo."""
 #     downloader = BaseDownloader()
-#     downloader.download(urls = "https://zenodo.org/records/"+record_id+"/files/"+record_name+"?download=1",
-#     paths = "./data/input/"+record_name)
+#     downloader.download("https://zenodo.org/records/10018590/files/enpkg_toy_dataset.zip?download=1","./data/input/enpkg_toy_dataset.zip")
 #     # ALSO EXTRACT ALL OF THE THINGS!
 #     extractor = AutoExtractor(delete_original_after_extraction=True)
 #     paths = glob("./data/input/**/*.zip", recursive=True)
 #     extractor.extract(paths)
+
+# Same function, with Zenodo record id as parameter
+
+def retrieve_zenodo_data(record_id, record_name):
+    """Retrieve the data from Zenodo.
+    Parameters
+    ----------
+    record_id : str
+        The Zenodo record ID.
+    record_name : str
+        The Zenodo record name.
+    """
+    downloader = BaseDownloader()
+    downloader.download(urls = "https://zenodo.org/records/"+record_id+"/files/"+record_name+"?download=1",
+    paths = "./data/input/"+record_name)
+    # ALSO EXTRACT ALL OF THE THINGS!
+    extractor = AutoExtractor(delete_original_after_extraction=True)
+    paths = glob("./data/input/**/*.zip", recursive=True)
+    extractor.extract(paths)
