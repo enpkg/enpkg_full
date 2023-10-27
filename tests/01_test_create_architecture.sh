2f45387b8634895d1e0b24945ee970b5c3e60908
grndstt <pierre-marie.allard@unige.ch>
2023-10-27 15:35:37 +0200

    up repro MSV

--- tests/01_test_create_architecture.sh
+++ tests/01_test_create_architecture.sh
@@ -1,8 +1,8 @@
 python ./01_enpkg_data_organization/src/create_architecture.py\
-    --source_path tests/data/input/enpkg_toy_dataset/msdata/processed \
+    --source_path tests/data/input/MSV000085119_pos/MSV000085119_pos/msdata/processed \
     --target_path tests/data/output\
-    --source_metadata_path tests/data/input/enpkg_toy_dataset/metadata \
-    --sample_metadata_filename metadata.tsv \
+    --source_metadata_path tests/data/input/MSV000085119_pos/MSV000085119_pos/metadata \
+    --sample_metadata_filename MSV000085119_metadata.tsv \
     --lcms_method_params_filename lcms_method_params.txt \
     --lcms_processing_params_filename lcms_processing_params.xml \
     --polarity pos
--- tests/02_test_massive_id.sh
+++ tests/02_test_massive_id.sh
@@ -1,3 +1,3 @@
 python ./01_enpkg_data_organization/src/add_massive_id.py\
-    --massive_id MSV000087728\
+    --massive_id MSV000093199\
     -p ./tests/data/output
