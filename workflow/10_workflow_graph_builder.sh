f5b3512e7a0b1fb495af362b42b37fb2e178af88
grndstt <pierre-marie.allard@unige.ch>
2023-11-27 18:08:20 +0100

    sirius para

--- workflow/10_workflow_graph_builder.sh
+++ workflow/10_workflow_graph_builder.sh
@@ -7,14 +7,14 @@ conda activate graph_builder
 # echo "Running 01_b_rdf_enpkgmodule_metadata_indi.py"
 # python ./06_enpkg_graph_builder/src/individual_processing/01_b_rdf_enpkgmodule_metadata_indi.py
 
-echo "Running 02_a_rdf_features_indi_para.py"
-python ./06_enpkg_graph_builder/src/individual_processing/02_a_rdf_features_indi_para.py
+# echo "Running 02_a_rdf_features_indi_para.py"
+# python ./06_enpkg_graph_builder/src/individual_processing/02_a_rdf_features_indi_para.py
 
 # echo "Running 02_b_rdf_features_spec2vec_indi_para.py"
 # python ./06_enpkg_graph_builder/src/individual_processing/02_b_rdf_features_spec2vec_indi_para.py
 
-# echo "Running 03_rdf_csi_annotations_indi.py"
-# python ./06_enpkg_graph_builder/src/individual_processing/03_rdf_csi_annotations_indi.py
+echo "Running 03_rdf_csi_annotations_indi_para.py"
+python ./06_enpkg_graph_builder/src/individual_processing/03_rdf_csi_annotations_indi_para.py
 
 # echo "Running 04_rdf_canopus_indi.py"
 # python ./06_enpkg_graph_builder/src/individual_processing/04_rdf_canopus_indi.py
