1e8f4c45dd8d96dbdbc90aafb464fb1602343d0f
grndstt <pierre-marie.allard@unige.ch>
2023-11-28 15:36:34 +0100

    wf KB

--- workflow/10_workflow_graph_builder.sh
+++ workflow/10_workflow_graph_builder.sh
@@ -4,8 +4,8 @@ conda activate graph_builder
 # echo "Running 01_a_rdf_enpkg_metadata_indi_para.py"
 # python ./06_enpkg_graph_builder/src/individual_processing/01_a_rdf_enpkg_metadata_indi_para.py
 
-echo "Running 01_b_rdf_enpkgmodule_metadata_indi_para.py"
-python ./06_enpkg_graph_builder/src/individual_processing/01_b_rdf_enpkgmodule_metadata_indi_para.py
+# echo "Running 01_b_rdf_enpkgmodule_metadata_indi_para.py"
+# python ./06_enpkg_graph_builder/src/individual_processing/01_b_rdf_enpkgmodule_metadata_indi_para.py
 
 # echo "Running 02_a_rdf_features_indi_para.py"
 # python ./06_enpkg_graph_builder/src/individual_processing/02_a_rdf_features_indi_para.py
@@ -13,8 +13,8 @@ python ./06_enpkg_graph_builder/src/individual_processing/01_b_rdf_enpkgmodule_m
 # echo "Running 02_b_rdf_features_spec2vec_indi_para.py"
 # python ./06_enpkg_graph_builder/src/individual_processing/02_b_rdf_features_spec2vec_indi_para.py
 
-# echo "Running 03_rdf_csi_annotations_indi_para.py"
-# python ./06_enpkg_graph_builder/src/individual_processing/03_rdf_csi_annotations_indi_para.py
+echo "Running 03_rdf_csi_annotations_indi_para.py"
+python ./06_enpkg_graph_builder/src/individual_processing/03_rdf_csi_annotations_indi_para.py
 
 # echo "Running 04_rdf_canopus_indi_para.py"
 # python ./06_enpkg_graph_builder/src/individual_processing/04_rdf_canopus_indi_para.py
