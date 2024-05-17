bca0b3d4517257be19ae4e1a07b7d2f4da13e8bb
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-05-17 14:35:59 +0200

    some scripts to manage mzML metadata

--- msmatch_tests.py
+++ msmatch_tests.py
@@ -1,19 +1,63 @@
+import re
+
+from matchms.importing import load_from_mzml
 from pyteomics import mzml
 
 def extract_injection_mode(mzml_file):
     injection_mode = None
     with mzml.read(mzml_file) as reader:
         for spectrum in reader:
+            print(spectrum)
+            #lowest_mz = spectrum["lowest observed m/z"]
+            #highest_mz = spectrum["highest observed m/z"]
+            #print(f"lowest m/z: {lowest_mz}")
+            #print(f"highest m/z: {highest_mz}")
             # Check if injection mode is available in spectrum metadata
             if 'positive scan' in spectrum:
                 injection_mode = "pos"
-                break
+                #break
             elif 'negative scan' in spectrum:
                 injection_mode = "neg"
-                break
+                #break
     return injection_mode
 
+def extract_last_spectrum(mzml_file):
+    spectrums = list(load_from_mzml(mzml_file))
+    last_entry = spectrums[-1].metadata
+    input_string = last_entry["title"]
+
+    # Regular expression to extract last scan
+    last_spectrum_pattern = r'scan=(\d+)'
+
+    # Find the scan number
+    last_spectrum_match = re.search(last_spectrum_pattern, input_string)
+    if last_spectrum_match:
+        last_spectrum = last_spectrum_match.group(1)
+    else:
+        last_spectrum = None
+    return last_spectrum
+
+def extract_source_file(mzml_file):
+    spectrums = list(load_from_mzml(mzml_file))
+    last_entry = spectrums[-1].metadata
+    input_string = last_entry["title"]
+
+    # Regular expression to extract filename
+    filename_pattern = r'File:"([^"]+)"'
+
+    # Find the filename
+    filename_match = re.search(filename_pattern, input_string)
+    if filename_match:
+        filename = filename_match.group(1)
+    else:
+        filename = None
+    return filename
+
 # Replace 'your_file.mzML' with the path to your .mzML file
-mzml_file = '202404101527_EB_dbgi_001189_01_01.mzML'
+mzml_file = '20240307_EB_dbgi_001195_01_01.mzML'
 injection_mode = extract_injection_mode(mzml_file)
+filename = extract_source_file(mzml_file)
+last_spectrum = extract_last_spectrum(mzml_file)
 print("Injection mode:", injection_mode)
+print(f'Filename: {filename}')
+print(f'Last spectrum: {last_spectrum}')
---
+++ xml_reader_test.py
@@ -0,0 +1,26 @@
+from lxml import etree
+
+# Parse the mzML file
+tree = etree.parse('20240307_EB_dbgi_001195_01_01.mzML')
+root = tree.getroot()
+
+# Define the namespaces
+namespaces = {
+    'mzml': 'http://psi.hupo.org/ms/mzml',
+    'xsi': 'http://www.w3.org/2001/XMLSchema-instance'
+}
+
+
+# Access the sourceFile element within fileDescription
+source_file = root.find('.//mzml:sourceFile', namespaces)
+cv_param = source_file.find('.//mzml:cvParam', namespaces)
+
+# Get the location attribute from the sourceFile element
+location = source_file.get('location')
+print(location)
+file = source_file.get('name')
+print(file)
+
+# Print the fileDescription element as a string
+print(etree.tostring(source_file, pretty_print=True).decode())
+print(etree.tostring(cv_param, pretty_print=True).decode())
