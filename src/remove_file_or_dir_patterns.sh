75eccc19dd551e0341187d7ea56db3ad2ea3f828
oolonek <pierre-marie.allard@unifr.ch>
2024-06-25 07:48:33 +0200

    garbahe coll for iterate taxa

--- src/remove_file_or_dir_patterns.sh
+++ src/remove_file_or_dir_patterns.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-# Script to recursively preview or delete files with a specified pattern in a given directory
+# Script to recursively preview or delete files and directories with a specified pattern in a given directory
 
 # Function to display help
 show_help() {
     echo "Usage: $0 [-p] <start_directory> <pattern>"
     echo
-    echo "This script recursively deletes or previews all files with a specified pattern"
+    echo "This script recursively deletes or previews all files and directories with a specified pattern"
     echo "within the given directory and its subdirectories."
     echo
     echo "Options:"
-    echo "  -p    Preview the files to be deleted, without actually deleting them."
+    echo "  -p    Preview the files and directories to be deleted, without actually deleting them."
     echo
     echo "Arguments:"
     echo "  start_directory: The directory where the search will begin."
     echo "                   This can be an absolute or a relative path."
-    echo "  pattern:         The file pattern of the files to delete or preview."
+    echo "  pattern:         The file and directory pattern to delete or preview."
     echo "                   Do not include the dot (.) before the pattern."
     echo
     echo "Example:"
     echo "  $0 /path/to/directory txt"
-    echo "  This will delete all .txt files in /path/to/directory and its subdirectories."
+    echo "  This will delete all .txt files and directories containing 'txt' in /path/to/directory and its subdirectories."
     echo
     echo "  $0 -p /path/to/directory txt"
-    echo "  This will preview all .txt files in /path/to/directory and its subdirectories."
+    echo "  This will preview all .txt files and directories containing 'txt' in /path/to/directory and its subdirectories."
 }
 
 # Check if at least two arguments are provided
@@ -51,12 +51,14 @@ fi
 START_DIR=$1
 PATTERN=$2
 
-# Find and either preview or remove files with the given pattern in the specified directory
+# Find and either preview or remove files and directories with the given pattern in the specified directory
 if $PREVIEW_MODE; then
-    echo "Previewing files to be deleted:"
-    find "$START_DIR" -type f -name "*$PATTERN*"
+    echo "Previewing files and directories to be deleted:"
+    find "$START_DIR" -type f -name "*$PATTERN*" -print
+    find "$START_DIR" -type d -name "*$PATTERN*" -print
 else
     find "$START_DIR" -type f -name "*$PATTERN*" -exec rm {} +
+    find "$START_DIR" -type d -name "*$PATTERN*" -exec rm -r {} +
 fi
 
 # The script ends here
--- workflow/03_workflow_taxo_enhancer.sh
+++ workflow/03_workflow_taxo_enhancer.sh
@@ -1,1 +1,2 @@
-python ./02_enpkg_taxo_enhancer/src/taxo_info_fetcher.py
+# python ./02_enpkg_taxo_enhancer/src/taxo_info_fetcher.py
+python ./02_enpkg_taxo_enhancer/src/taxo_utils_caller.py
