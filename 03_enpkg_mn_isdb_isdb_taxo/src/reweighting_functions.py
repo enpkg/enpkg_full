d51775a8cf0302b9de7bc0701d939a8f5cb9e7e7
oolonek <pierre-marie.allard@unifr.ch>
2024-09-25 14:25:10 +0200

    npc classification and special chracters escaping

--- 03_enpkg_mn_isdb_isdb_taxo/src/reweighting_functions.py
+++ 03_enpkg_mn_isdb_isdb_taxo/src/reweighting_functions.py
@@ -91,9 +91,7 @@ def taxonomical_reponderator(dt_isdb_results, min_score_taxo_ms1):
 
 
 def chemical_reponderator(
-    clusterinfo_summary_file,
-    dt_isdb_results,
-    top_N_chemical_consistency
+    clusterinfo_summary_file, dt_isdb_results, top_N_chemical_consistency
 ):
     """Perform chemical consistency reweighting on a list of candidates annotations
 
--- monolith/monolith/utils/__init__.py
+++ monolith/monolith/utils/__init__.py
@@ -2,5 +2,10 @@
 
 from monolith.utils.binary_search import binary_search_by_key
 from monolith.utils.label_propagation_algorithm import label_propagation_algorithm
+from monolith.utils.npc_fetcher import get_classification_results
 
-__all__ = ["binary_search_by_key", "label_propagation_algorithm"]
+__all__ = [
+    "binary_search_by_key",
+    "label_propagation_algorithm",
+    "get_classification_results",
+]
